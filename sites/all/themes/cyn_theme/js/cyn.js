a241ef7b759d8dbcb5ca74296a13585e6c5ebdda
Brian Sullivan <bri_sull@yahoo.com>
2012-03-11 10:39:02 -0700

    An honest day's work. Tons of CSS updates, calendar functionality... kind of.

---
+++ sites/all/themes/cyn_theme/js/cyn.js
@@ -0,0 +1,16 @@
+// alert("here");
+
+jQuery(document).ready( function() {
+        
+    jQuery("#main-content").css("height",jQuery("#columns").css("height") );
+    jQuery(window).resize(function() {
+        console.log( jQuery(window).width() );
+        if ( parseInt( jQuery(window).width() ) < 768  ) {
+            jQuery("#main-content").css( "height",jQuery("#main-content .block-content").css("height" ) );
+        }
+        else {
+            jQuery("#main-content").css("height",jQuery("#columns").css("height") );
+        }
+});
+   // alert( jQuery("#main-content").css("height") );
+});
