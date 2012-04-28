f4ceed7753dc34e64909cd9a957b9141c5c34e3a
Brian Sullivan <bri_sull@yahoo.com>
2012-04-27 23:15:49 -0700

    Detect overlays

--- sites/all/themes/cyn_theme/js/cyn.js
+++ sites/all/themes/cyn_theme/js/cyn.js
@@ -20,4 +20,29 @@ jQuery(document).ready( function() {
    jQuery(".page-node").not(".page-node-67").find('a[href^="mailto:"]').each( function() {
         jQuery(this).attr("href","/about-us/contact-city-youth-now");
     });
+   
+   
+   
+   var overlayScript = function() {
+       var self = this;
+       self.checkForAddDonation = (new RegExp("node/add/donation").test(document.location.hash));
+       self.checkForEditDonation = (new RegExp("overlay-context=node").test(document.location.hash) && jQuery("body").hasClass("page-type-donation") ) ;
+       
+       self.donation = function() {
+           return ( self.checkForAddDonation || self.checkForEditDonation );
+       };
+   };
+   var overlay = new overlayScript();
+   
+   if ( overlay.donation() ){
+       /*
+       var dateSelect = jQuery("#overlay-active").contents().find(".date-year").find("select");
+       var donorSelect = jQuery("#overlay-active").contents().find("#edit-field-donor select");
+       alert( dateSelect.html() );
+           jQuery(dateSelect, donorSelect).bind("change", function() {
+                   alert("hay");
+                   jQuery("overlay-active").contents().find("#edit-title").val( dateSelect.val() +": "+ donorSelect.val() ); 
+           });
+           */
+   }
 });
